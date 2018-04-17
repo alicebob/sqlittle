c5e9b97502b36c57b81e1a28d62b1bc860ef9da1
Harmen <alicebob@lijzij.de>
2018-04-17 22:07:27 +0200

    test files

---
+++ test/worowid.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -eu
+
+DB=./worowid.sqlite
+
+rm -f $DB
+(
+    echo "BEGIN;"
+    echo "CREATE TABLE words (word varchar primary key, length int) WITHOUT ROWID;"
+    for w in $( cat words.txt ); do
+        echo "INSERT INTO words VALUES (\"$w\", length(\"$w\"));"
+    done
+    echo "CREATE INDEX words_l ON words (length, word);"
+    echo "COMMIT;"
+) | sqlite3 --batch $DB
