65ed73f684c258a9c125d0bb463cb17c13ffbc85
Harmen <alicebob@lijzij.de>
2018-05-04 14:00:26 +0200

    clean up test

--- sql/tokenizer_test.go
+++ sql/tokenizer_test.go
@@ -7,119 +7,124 @@ import (
 )
 
 func TestTokens(t *testing.T) {
-	type cas struct {
-		sql  string
-		want []token
-		err  error
-	}
-	for n, c := range []cas{
-		{
-			sql: "foo foo_bar FoObAr foo1 _foo café",
-			want: []token{
-				stoken(tBare, "foo"),
-				stoken(tBare, "foo_bar"),
-				stoken(tBare, "FoObAr"),
-				stoken(tBare, "foo1"),
-				stoken(tBare, "_foo"),
-				stoken(tBare, "café"),
-			},
-		},
-		{
-			sql: "1 -12 +34",
-			want: []token{
-				ntoken(tSignedNumber, 1),
-				ntoken(tSignedNumber, -12),
-				ntoken(tSignedNumber, +34),
-			},
-		},
-		{
-			sql: "create table foo",
-			want: []token{
-				stoken(CREATE, "create"),
-				stoken(TABLE, "table"),
-				stoken(tBare, "foo"),
-			},
-		},
-		{
-			sql: "create table foo (col1, col2, col3)",
-			want: []token{
-				stoken(CREATE, "create"),
-				stoken(TABLE, "table"),
-				stoken(tBare, "foo"),
-				stoken('(', "("),
-				stoken(tBare, "col1"),
-				stoken(',', ","),
-				stoken(tBare, "col2"),
-				stoken(',', ","),
-				stoken(tBare, "col3"),
-				stoken(')', ")"),
-			},
-		},
-		{
-			// *
-			sql: "select * from foo",
-			want: []token{
-				stoken(SELECT, "select"),
-				stoken(tOperator, "*"),
-				stoken(FROM, "from"),
-				stoken(tBare, "foo"),
-			},
-		},
-		{
-			// fancy whitespace
-			sql: "  \tselect\n*\nfrom   foo ",
-			want: []token{
-				stoken(SELECT, "select"),
-				stoken(tOperator, "*"),
-				stoken(FROM, "from"),
-				stoken(tBare, "foo"),
-			},
-		},
-		{
-			sql: "from FROM 'from' ''",
-			want: []token{
-				stoken(FROM, "from"),
-				stoken(FROM, "FROM"),
-				stoken(tLiteral, "from"),
-				stoken(tLiteral, ""),
-			},
-		},
-		{
-			sql: "bare \"id 1\" [id 2] `id 3` 'lit 1'",
-			want: []token{
-				stoken(tBare, "bare"),
-				stoken(tIdentifier, "id 1"),
-				stoken(tIdentifier, "id 2"),
-				stoken(tIdentifier, "id 3"),
-				stoken(tLiteral, "lit 1"),
-			},
-		},
-		{
-			sql: "|| | * > >=",
-			want: []token{
-				stoken(tOperator, "||"),
-				stoken(tOperator, "|"),
-				stoken(tOperator, "*"),
-				stoken(tOperator, ">"),
-				stoken(tOperator, ">="),
-			},
-		},
-		{
-			sql: "foo 'bar",
-			err: errors.New("no terminating ' found"),
-		},
-	} {
-		ts, err := tokenize(c.sql)
-		if have, want := err, c.err; !reflect.DeepEqual(have, want) {
-			t.Errorf("case %d: have %#v, want %#v", n, have, want)
-			continue
+	testOK := func(sql string, want []token) {
+		t.Helper()
+		ts, err := tokenize(sql)
+		if err != nil {
+			t.Error(err)
+			return
 		}
-		if c.err != nil {
-			continue
-		}
-		if have, want := ts, (c.want); !reflect.DeepEqual(have, want) {
-			t.Errorf("case %d: have %#v, want %#v", n, have, want)
+		if have := ts; !reflect.DeepEqual(have, want) {
+			t.Errorf("have %#v, want %#v", have, want)
 		}
 	}
+	testError := func(sql string, want error) {
+		t.Helper()
+		_, err := tokenize(sql)
+		if have := err; !reflect.DeepEqual(have, want) {
+			t.Errorf("have %#v, want %#v", have, want)
+		}
+	}
+
+	testOK(
+		"foo foo_bar FoObAr foo1 _foo café",
+		[]token{
+			stoken(tBare, "foo"),
+			stoken(tBare, "foo_bar"),
+			stoken(tBare, "FoObAr"),
+			stoken(tBare, "foo1"),
+			stoken(tBare, "_foo"),
+			stoken(tBare, "café"),
+		},
+	)
+
+	testOK(
+		"1 -12 +34",
+		[]token{
+			ntoken(tSignedNumber, 1),
+			ntoken(tSignedNumber, -12),
+			ntoken(tSignedNumber, +34),
+		},
+	)
+	testOK(
+		"create table foo",
+		[]token{
+			stoken(CREATE, "create"),
+			stoken(TABLE, "table"),
+			stoken(tBare, "foo"),
+		},
+	)
+
+	testOK(
+		"create table foo (col1, col2, col3)",
+		[]token{
+			stoken(CREATE, "create"),
+			stoken(TABLE, "table"),
+			stoken(tBare, "foo"),
+			stoken('(', "("),
+			stoken(tBare, "col1"),
+			stoken(',', ","),
+			stoken(tBare, "col2"),
+			stoken(',', ","),
+			stoken(tBare, "col3"),
+			stoken(')', ")"),
+		},
+	)
+	// *
+	testOK(
+		"select * from foo",
+		[]token{
+			stoken(SELECT, "select"),
+			stoken(tOperator, "*"),
+			stoken(FROM, "from"),
+			stoken(tBare, "foo"),
+		},
+	)
+
+	// fancy whitespace
+	testOK(
+		"  \tselect\n*\nfrom   foo ",
+		[]token{
+			stoken(SELECT, "select"),
+			stoken(tOperator, "*"),
+			stoken(FROM, "from"),
+			stoken(tBare, "foo"),
+		},
+	)
+	testOK(
+		"from FROM 'from' ''",
+		[]token{
+			stoken(FROM, "from"),
+			stoken(FROM, "FROM"),
+			stoken(tLiteral, "from"),
+			stoken(tLiteral, ""),
+		},
+	)
+
+	testOK(
+		"bare \"id 1\" [id 2] `id 3` 'lit 1'",
+		[]token{
+			stoken(tBare, "bare"),
+			stoken(tIdentifier, "id 1"),
+			stoken(tIdentifier, "id 2"),
+			stoken(tIdentifier, "id 3"),
+			stoken(tLiteral, "lit 1"),
+		},
+	)
+	testOK(
+		"|| | * > >=",
+		[]token{
+			stoken(tOperator, "||"),
+			stoken(tOperator, "|"),
+			stoken(tOperator, "*"),
+			stoken(tOperator, ">"),
+			stoken(tOperator, ">="),
+		},
+	)
+
+	testError(
+		"foo 'bar",
+		errors.New("no terminating ' found"),
+	)
 
 }
